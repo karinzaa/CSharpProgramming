a67d38f2d4758d2e37460148192d16f43a393338
Karinzaa Vitoonkijwanit <minkarin447@gmail.com>
2019-07-02 13:08:38 +0700

    Add TestCode

---
+++ TestCode/TestCode1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int price = 299;
+            Console.Write("Input number of customer: ");
+            int custom = int.Parse(Console.ReadLine());
+            double total = custom  * price;
+            Console.WriteLine();
+            Console.WriteLine("Total price {0,14:n2} B.", total);
+            double vat = (total*3)/100;
+            Console.WriteLine("Add water 3%{0,14:n2} B.", vat);
+            double totalall = vat + total;
+            Console.WriteLine("Total price {0,14:n2} B.", totalall);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TestCode/TestCode1/ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input employee salary : ");
+            int salary= int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("SALARY {0,10:n2} ", salary);
+            double social = (salary * 3) / 100;
+            Console.WriteLine("SOCIAL {0,10:n2} ", social);
+            double tax = (salary * 4) / 100;
+            Console.WriteLine("TAX {0,13:n2}", tax);
+            double salarySum = salary - social - tax;
+            Console.WriteLine("EMPLOYEE SALARY = {0:n2}", salarySum);
+            Console.ReadKey();
+        }
+    }
+}
