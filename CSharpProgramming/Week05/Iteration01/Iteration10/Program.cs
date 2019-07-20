7995736a1d99d1ed9e133e3b56f335c887a4bb14
Karinzaa Vitoonkijwanit <minkarin447@gmail.com>
2019-07-20 09:04:06 +0700

    Add Week05

---
+++ CSharpProgramming/Week05/Iteration01/Iteration10/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iteration10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Input a number : ");
+            int val = int.Parse(Console.ReadLine());
+            Console.Write("Input a number : ");
+            int x = int.Parse(Console.ReadLine());
+            int i, j, k;
+
+            switch(x)
+            {
+                case 1:
+                    for (i = 1; i <= val; i++)
+                    {
+                        for (j = 1; j <= i; j++)
+                        {
+                            Console.Write("");
+                        }
+                        for (k = 1; k <= i; k++)
+                        {
+                            Console.Write("*");
+                        }
+                        Console.WriteLine("");
+                    }
+                    Console.ReadLine();
+                    break;
+
+                case 2:
+                    for (i = 1; i <= val; i++)
+                    {
+                        for (j = 1; j <= val - i; j++)
+                        {
+                            // Console.Write(" ");  
+                        }
+                        for (k = 1; k <= j; k++)
+                        {
+                            Console.Write("*");
+                        }
+                        Console.WriteLine("");
+                    }
+                    Console.ReadLine();
+
+                    break;
+
+                case 3:
+                    for (i = 1; i >= val; i--)
+                    {
+                        for (j = 1; j >= val - i; j++)
+                        {
+                            Console.Write("*");
+                        }
+                        for (k = val; k >= val; val--)
+                        {
+                            Console.Write(" ");
+                        }
+                        Console.WriteLine("");
+                    }
+                    Console.ReadLine();
+                    break;
+
+                case 4:
+                    for (i = 1; i <= val; i++)
+                    {
+                        for (j = 1; j <= val - i; j++)
+                        {
+                            Console.Write(" ");
+                        }
+                        for (k = 1; k <= i; k++)
+                        {
+                            Console.Write("*");
+                        }
+                        Console.WriteLine("");
+                    }
+                    Console.ReadLine();
+                    break;
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
