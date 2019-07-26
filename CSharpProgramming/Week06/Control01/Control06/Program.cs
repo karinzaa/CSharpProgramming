28c66b4ddc36aba914cdbef9bcbd13e2af6bf210
Karinzaa Vitoonkijwanit <minkarin447@gmail.com>
2019-07-26 14:30:08 +0700

    Add Week06

---
+++ CSharpProgramming/Week06/Control01/Control06/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Control06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = 0, i = 0, sum = 0;
+
+                Console.Write($"input a positive number : ");
+                n = double.Parse(Console.ReadLine());
+                while (n <= 0)
+                {
+                    Console.Write($"Please,input a positive number only again : ");
+                    n = double.Parse(Console.ReadLine());
+                }
+            Console.WriteLine("");
+            for (i = 1; i <= n; i++)
+            {
+                sum += i;
+                Console.Write($"{i}");
+               
+                    if ( i < n)
+                    {
+
+                      Console.Write($" + ");
+                    }
+                   
+                }
+            Console.WriteLine($" = {sum}");
+            Console.ReadKey();
+        }
+
+            
+        }
+    }
+
