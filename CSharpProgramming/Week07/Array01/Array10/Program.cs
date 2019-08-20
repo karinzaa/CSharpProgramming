9e3035b18a0f6f37464a222eab5719f23b881074
Karinzaa Vitoonkijwanit <minkarin447@gmail.com>
2019-08-20 11:07:07 +0700

    Add Week07

---
+++ CSharpProgramming/Week07/Array01/Array10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input a message :");
+            string message = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Show in upper case = ");
+            foreach (Char _mes in message)
+            {
+                if (_mes >= 97 && _mes <= 122)
+                {
+                    Console.Write((char)(_mes-32));
+                  
+                }
+                else
+
+                    Console.Write(_mes);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
